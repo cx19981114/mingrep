d70442935930438658320f2d769810ef94d64202
cx <17367073440@163.com>
2020-10-12 11:02:49 +0800

    modify lib.rs and cargo.toml about lib

--- src/lib.rs
+++ src/lib.rs
@@ -58,29 +58,28 @@ pub fn search_case_insensitive<'a>(query:&str,contents:&'a str)
     contents.lines().filter(|a|a.to_lowercase().
     contains(&query.to_lowercase())).collect()
 }
-
-// #[cfg(test)]
-// mod tests{
-//     use super::*;
-//     #[test]
-//     fn case_sensitive(){
-//         let query = "duct";
-//         let contents="\
-// Rust:
-// safe,fast,productive.
-// Pick three.
-// Duct tape";
-//         assert_eq!(vec!["safe,fast,productive."],search(query,contents));
-//     }
-//     #[test]
-//     fn case_insensitive(){
-//         let query = "rUsT";
-//         let contents="\
-// Rust:
-// safe,fast,productive.
-// Pick three.
-// Trust me.";
-//         assert_eq!(vec!["Rust:", "Trust me."],
-//         search_case_insensitive(query,contents));
-//     }
-// }
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn case_sensitive(){
+        let query = "duct";
+        let contents="\
+Rust:
+safe,fast,productive.
+Pick three.
+Duct tape";
+        assert_eq!(vec!["safe,fast,productive."],search(query,contents));
+    }
+    #[test]
+    fn case_insensitive(){
+        let query = "rUsT";
+        let contents="\
+Rust:
+safe,fast,productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."],
+        search_case_insensitive(query,contents));
+    }
+}
